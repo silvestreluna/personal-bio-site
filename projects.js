564c5fa21df19bdf4a5a059ae5e88b65a4ead6d1
Silvestre Luna <slvtrluna@gmail.com>
2019-03-09 08:46:02 -0600

    adding more stuff

--- projects.js
+++ projects.js
@@ -17,6 +17,44 @@ const projects = [
           url: "https://www.google.com", 
           githubUrl: "https://github.com"
           
+      },
+      {
+        title: "Project2", 
+        screenshot: "https://picsum.photos/200/200", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: true,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/silvestreluna/product_cards"
+      }, 
+      {
+          title: "project3", 
+          screenshot: "https://picsum.photos/200/200", 
+          description: "Somehting something something cool", 
+          technologiesUsed: "HTML/CSS/JS",
+          available: true, 
+          url: "https://www.google.com", 
+          githubUrl: "https://github.com"
+          
+      },
+      {
+        title: "project4", 
+        screenshot: "https://picsum.photos/200/200", 
+        description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+        technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+        available: false,
+        url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+        githubUrl: "https://github.com/silvestreluna/product_cards"
+      }, 
+      {
+          title: "project15",
+          screenshot: "https://picsum.photos/200/200", 
+          description: "Somehting something something cool", 
+          technologiesUsed: "HTML/CSS/JS",
+          available: true, 
+          url: "https://www.google.com", 
+          githubUrl: "https://github.com"
+          
       }
 ];
 
@@ -36,8 +74,8 @@ const createProjectCards = () => {
             domString += `<img src="${projects[i].screenshot}">`;
             domString += `<p>${projects[i].description}</p>`;
             domString += `<p>${projects[i].technologiesUsed}</p>`;
-            domString += `<p>${projects[i].url}</p>`;
-            domString += `<p>${projects[i].githubUrl}</p>`;
+            domString += `<a href="${projects[i].url}">Link</a> <br>`;
+            domString += `<a href="${projects[i].githubUrl}">GitHub</a>`;
             domString += `</div>`
         };
     
